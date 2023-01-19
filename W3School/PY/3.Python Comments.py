27b2bddc1b0d75db02abd571f31a70de799270df
vmfmgl0607 <116646670+vmfmgl0607@users.noreply.github.com>
2023-01-19 15:23:51 +0900

    Add files via upload

---
+++ W3School/PY/3.Python Comments.py
@@ -0,0 +1,20 @@
+#this is a comment.
+print("Hello, World!")
+
+print("Hello, World!") #this is a comment
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#this is a comment.
+#written in
+#more than just one line
+print("Hello, World")
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World")
---
+++ W3School/PY/6.Python Variables - Assign Multiple Values.py
@@ -0,0 +1,24 @@
+x ,y, z = "orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+print("---------------------------------------")
+
+x = y = z = "Orange"
+
+print(x)
+print(y)
+print(z)
+
+print("---------------------------------------")
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
+
+print("---------------------------------------")
